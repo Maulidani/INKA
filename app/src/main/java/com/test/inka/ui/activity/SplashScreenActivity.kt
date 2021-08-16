4a622012c6493933fed37d2d8d315ea18995c2e4
Maulidani <maulidanimahmud@gmail.com>
2021-08-16 13:30:37 +0800

    Initial commit

---
+++ app/src/main/java/com/test/inka/ui/activity/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.test.inka.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test.inka.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
